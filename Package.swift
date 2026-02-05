8ea4755a7b916659d06966f71697ba09b41711d8
German Arutyunov <germanarutyunov@gmail.com>
2026-02-05 01:57:56 +0100

    fix(desktop): use mlx-swift-lm 2.29.3 for correct MLXLLM/MLXVLM/MLXEmbedders deps
    Replace mlx-swift (low-level) with mlx-swift-lm (high-level) which provides
    MLXLLM, MLXVLM, MLXEmbedders, MLXLMCommon. Pin to 2.29.x to avoid Swift 6
    strict concurrency bug in 2.30.x LoRAContainer.swift.

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "Gophy",
     platforms: [
-        .macOS(.v14)
+        .macOS("14.4")
     ],
     products: [
         .executable(
@@ -13,8 +13,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/ml-explore/mlx-swift", from: "0.30.0"),
-        .package(url: "https://github.com/huggingface/swift-transformers", from: "1.1.0"),
+        .package(url: "https://github.com/ml-explore/mlx-swift-lm", .upToNextMinor(from: "2.29.3")),
         .package(url: "https://github.com/argmaxinc/WhisperKit", from: "0.9.0"),
         .package(url: "https://github.com/groue/GRDB.swift", from: "7.0.0")
     ],
@@ -22,13 +21,10 @@ let package = Package(
         .executableTarget(
             name: "Gophy",
             dependencies: [
-                .product(name: "MLX", package: "mlx-swift"),
-                .product(name: "MLXNN", package: "mlx-swift"),
-                .product(name: "MLXRandom", package: "mlx-swift"),
-                .product(name: "MLXFast", package: "mlx-swift"),
-                .product(name: "MLXOptimizers", package: "mlx-swift"),
-                .product(name: "MLXFFT", package: "mlx-swift"),
-                .product(name: "Transformers", package: "swift-transformers"),
+                .product(name: "MLXLLM", package: "mlx-swift-lm"),
+                .product(name: "MLXVLM", package: "mlx-swift-lm"),
+                .product(name: "MLXEmbedders", package: "mlx-swift-lm"),
+                .product(name: "MLXLMCommon", package: "mlx-swift-lm"),
                 .product(name: "WhisperKit", package: "WhisperKit"),
                 .product(name: "GRDB", package: "GRDB.swift")
             ]
